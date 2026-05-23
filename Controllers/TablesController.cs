193f09262dae0b4e3c0df1d46118aba745cd9589
Tomki2258 <tomek.rybinski@interia.pl>
2026-05-23 15:38:47 +0200

    improved tables managment

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -64,11 +64,11 @@ public class TablesController :  ControllerBase
     
     [Authorize(Roles = "Admin")]
     [HttpPost("new")]
-    public ActionResult<int> CreateTable([FromBody] PostTableBody tableBody)
+    public async Task<ActionResult<int>> CreateTable([FromBody] PostTableBody tableBody)
     {
         try
         {
-            var o = _tablesService.AddTable(tableBody);
+            var o = await _tablesService.AddTableAsync(tableBody);
             if (o != -1)
             {
                 return Ok(o);
@@ -83,10 +83,12 @@ public class TablesController :  ControllerBase
     }
 
     [Authorize(Roles = "Admin")]
-    [HttpDelete("{id}/remove")]
-    public ActionResult<int> RemoveTable(int id)
+    [HttpPatch("{id}/remove")]
+    public async Task<ActionResult<int>> RemoveTable(int id)
     {
-        _tablesService.RemoveTable(id);
-        return Ok();    // nwm czy coś więcej powinniśmy dawać
+        var r = await _tablesService.SetTableStatus(id, "Paid");
+        if (r)
+            return Ok();
+        return BadRequest("Table not found");
     }
 }
--- Services/Interfaces/ITablesService.cs
+++ Services/Interfaces/ITablesService.cs
@@ -10,6 +10,6 @@ public interface ITablesService
     public Task<List<Order>> GetTableOrders(int id);
     public Task<bool> SetTableStatus(int id, string status);
     public Task<bool> ClearTable(int id);
-    public int AddTable(PostTableBody table);
+    public Task<int> AddTableAsync(PostTableBody table);
     public void RemoveTable(int id);
 }
